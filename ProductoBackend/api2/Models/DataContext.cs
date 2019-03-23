4580a4f0659ce0af6fa493c513ef69acdda24eca
Samuel Peña <samuel_pena@hotmail.es>
2019-03-23 11:26:07 -0400

    API menos AZURE

---
+++ ProductoBackend/api2/Models/DataContext.cs
@@ -0,0 +1,13 @@
+﻿namespace api2.Models
+{
+    using System.Data.Entity;
+    public class DataContext:DbContext
+    {
+        public DataContext():base("DefaultConnection")
+        {
+
+        }
+
+        public System.Data.Entity.DbSet<api2.Models.Product> Products { get; set; }
+    }
+}
